8996604e647c077504b1806349d9a4696f80f8eb
jakubsb <jakub.bernecki01@gmail.com>
2024-04-09 18:37:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
  
- 
+
 @app.route('/prediction', methods=['POST'])
 def predict_image_file():
     try:
         if request.method == 'POST':
             input_tensor = preprocess_image(request.files['file'].stream)
             pred, acc = predict_class(input_tensor)
-            output = f"I'm {acc*100:.2f}% sure it's {pred}"
+            output = f"I'm {acc*100:.0f}% sure it's {pred}"
             return render_template("result.html", predictions=output)
  
     except:
@@ -23,4 +23,4 @@ def predict_image_file():
  
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
